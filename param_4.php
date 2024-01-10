0d30c5acfb4667a62f2f725c242238ad3d127043
Raj <155172511+2003Nilu@users.noreply.github.com>
2024-01-10 00:55:05 -0800

    n2

---
+++ param_4.php
@@ -0,0 +1,14 @@
+<?php
+$s=$_GET["s"];
+$r=$_GET["c"];
+    if($s=="submit")
+             echo " Resistration Fail ";
+
+    else
+    {
+                echo " SucessFully Resistration ";
+            
+    }
+    
+?>
+
